5b59e204f562dbd36c94f0ba79cb4bdc7b31348e
GabytaDev <gabyta.loyola@gmail.com>
2023-03-16 18:51:09 -0300

    Initial Commit

---
+++ ModalSwiftUI/ModalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalSwiftUIApp.swift
+//  ModalSwiftUI
+//
+//  Created by Gabriela Loyola on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ModalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
